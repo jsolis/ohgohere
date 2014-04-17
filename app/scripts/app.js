6d63d46cef7853da8a7c74fa2918ddde4102c087
Jason Solis <jsolis@gmail.com>
2014-04-16 20:15:59 -0400

    stubbed out inspire view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,10 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/inspire', {
+        templateUrl: 'views/inspire.html',
+        controller: 'InspireCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/inspire.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ohgohereApp')
+  .controller('InspireCtrl', function ($scope, $firebase) {
+  	
+  });
