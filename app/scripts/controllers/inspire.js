993555bfd589a5d3d93bcde6bae90a6fd8a3d094
Jason Solis <jsolis@gmail.com>
2014-04-17 20:08:14 -0400

    finished adding new itin screen

--- app/scripts/controllers/inspire.js
+++ app/scripts/controllers/inspire.js
@@ -2,5 +2,29 @@
 
 angular.module('ohgohereApp')
   .controller('InspireCtrl', function ($scope, $firebase) {
-  	
+    var itineraries = new Firebase('https://ohgohere.firebaseio.com/itinerary');
+    $scope.itineraries = $firebase(itineraries);
+
+    $scope.postItin = function() {
+      console.log($scope.itin.location);
+      $scope.itineraries.$add({
+        location: $scope.itin.location,
+        when: $scope.itin.when,
+        who: $scope.itin.who,
+        places: [
+          {
+            name: $scope.itin.place1.name,
+            why: $scope.itin.place1.why
+          },
+          {
+            name: $scope.itin.place2.name,
+            why: $scope.itin.place2.why
+          },
+          {
+            name: $scope.itin.place3.name,
+            why: $scope.itin.place3.why
+          }
+        ]
+      });
+    };
   });
