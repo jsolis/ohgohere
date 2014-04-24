4b29bbf33b745cbfb5e657a91738e63f5726eb35
Jason Solis <jsolis@gmail.com>
2014-04-23 21:34:16 -0400

    poor man's search but it works

--- app/scripts/filters/search-itin-filter.js
+++ app/scripts/filters/search-itin-filter.js
@@ -3,7 +3,32 @@
 angular.module('ohgohereApp')
   .filter('searchItin', function () {
     return function(input, searchTerm) {
-      var filtered = _.filter(input, {'location': searchTerm});
-      return filtered;
+      return _.filter(input, function(itin) {
+        var match = false;
+        var terms = searchTerm.split(' ');
+        _.forEach(terms, function(term) {
+          if (itin.location.indexOf(term) > -1 || term.indexOf(itin.location) > -1 ||
+              itin.who.indexOf(term) > -1 || term.indexOf(itin.who) > -1 ||
+              itin.when.indexOf(term) > -1 || term.indexOf(itin.when) > -1) {
+            match = true;
+            return false;
+          }
+          var placeNames = _.flatten(itin.places, 'name');
+          _.forEach(placeNames, function(placeName) {
+            if (_.contains(placeName, term) || _.contains(term, placeName)) {
+              match = true;
+              return false;
+            }
+          });
+          var placeWhys = _.flatten(itin.places, 'why');
+          _.forEach(placeWhys, function(placeWhy) {
+            if (_.contains(placeWhy, term) || _.contains(term, placeWhy)) {
+              match = true;
+              return false;
+            }
+          });
+        });
+        return match;
+      });
     };
   });
