46e69019bd75e8346c16320529b08e6faf71d49e
Jason Solis <jsolis@gmail.com>
2014-08-08 11:43:44 -0400

    setting html5Mode to true and defining a rewrite in the firebase.json

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,8 @@ angular
     'ngRoute',
     'firebase'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
